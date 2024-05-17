a9a5e4137616723bb5905c4b1f87afd04907b00f
Harshit <98891064+Harshit26042004@users.noreply.github.com>
2024-05-17 20:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ server = app.server
 month_dict = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
 dict_month = {1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
 
-enc = pickle.load(open("/encoder.sav","rb"))
-model = keras.models.load_model("/modle.keras")
+enc = pickle.load(open("encoder.sav","rb"))
+model = keras.models.load_model("modle.keras")
 
 
 app.layout = html.Div([
